264b1adfdb9d78d8f7e6714cfad4f65354ee8427
Christian magallanes <desarrollowebfront@gmail.com>
2023-11-15 12:25:45 -0300

    gg

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -2,19 +2,19 @@ import React, { useState, useEffect, useRef } from "react";
 import "./focus.css";
 
 const Pomodoro = () => {
-    const storedTime = JSON.parse(localStorage.getItem("pomodoroTime")) || { minutes: 60, seconds: 0 };
-
-    const [minutes, setMinutes] = useState(storedTime.minutes);
-    const [seconds, setSeconds] = useState(storedTime.seconds);
+    const [minutes, setMinutes] = useState(30);
+    const [seconds, setSeconds] = useState(0);
     const [isActive, setIsActive] = useState(false);
     const intervalRef = useRef(null);
     const url = "https://open.spotify.com/embed/album/35DIb6j45YmbHAfyN7HgaG?utm_source=generator&theme=0";
+
     useEffect(() => {
         if (isActive) {
             intervalRef.current = setInterval(() => {
                 if (seconds === 0) {
                     if (minutes === 0) {
-                        clearInterval(intervalRef.current);
+                        setMinutes(30);
+                        setSeconds(0);
                         setIsActive(false);
                     } else {
                         setMinutes((prevMinutes) => prevMinutes - 1);
@@ -40,16 +40,7 @@ const Pomodoro = () => {
         }
     };
 
-    const continueTimer = () => {
-        setIsActive(true);
-    };
-
-    useEffect(() => {
-        // Almacena el tiempo en localStorage cuando se pausa
-        if (!isActive) {
-            localStorage.setItem("pomodoroTime", JSON.stringify({ minutes, seconds }));
-        }
-    }, [isActive, minutes, seconds]);
+  
 
     return (
         <div className="focus-container">
@@ -58,13 +49,12 @@ const Pomodoro = () => {
                 {String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}
             </p>
             <div className="btn-container">
-                <button className="btn" onClick={toggleTimer}>{isActive ? 'Pause' : 'Start'}</button>
-                <button className="btn" onClick={continueTimer} disabled={isActive}>
-                    Continue
-                </button>
+                <button className="btn" onClick={toggleTimer}>{isActive ? 'Pausar' : 'Iniciar'}</button>
+               
             </div>
-            <div className="ifm" >
-                <iframe className="if"
+            <div className="ifm">
+                <iframe
+                    className="if"
                     title="Contenido embebido único"
                     width="auto"
                     height="auto"
@@ -73,9 +63,7 @@ const Pomodoro = () => {
                     allowFullScreen
                 ></iframe>
             </div>
-
         </div>
-
     );
 };
 
